63f13e9ab2565b9b8c1b8da8dff3b7bc769260eb
Stef B <regendo@users.noreply.github.com>
2020-04-25 02:48:30 +0200

    Early font rendering & too many image libraries

--- src/card.rs
+++ src/card.rs
@@ -1,4 +1,7 @@
+use image::{DynamicImage, ImageBuffer, Rgba};
+use imageproc::drawing::draw_text_mut;
 use raster::{editor, BlendMode, PositionMode, ResizeMode};
+use rusttype::{Font, FontCollection, Scale};
 
 pub fn generate_example_card() {
 	let black_background = raster::open("design/exports/Background.png").unwrap();
@@ -32,7 +35,7 @@ pub fn generate_example_card() {
 	)
 	.unwrap();
 
-	let card = editor::blend(
+	let mut card = editor::blend(
 		&card,
 		&profession,
 		BlendMode::Normal,
@@ -43,5 +46,35 @@ pub fn generate_example_card() {
 	)
 	.unwrap();
 
-	raster::save(&card, "card.png");
+	let font_data = Vec::from(include_bytes!("../Roboto-Regular.ttf") as &[u8]);
+	let font: Font = FontCollection::from_bytes(&font_data)
+		.unwrap()
+		.into_font()
+		.unwrap();
+
+	let card_name = "Shadow Form";
+	let mut text_image =
+		ImageBuffer::from_raw(card.width as u32, card.height as u32, card.bytes).unwrap();
+	let type_line = "Elite Assassin Enchantment Spell (Shadow Arts)";
+	draw_text_mut(
+		&mut text_image,
+		Rgba([0x0_u8, 0x0_u8, 0x0_u8, 0xFF_u8]),
+		35,
+		275,
+		Scale::uniform(25.0),
+		&font,
+		card_name,
+	);
+	draw_text_mut(
+		&mut text_image,
+		Rgba([0x0_u8, 0x0_u8, 0x0_u8, 0xFF_u8]),
+		25,
+		315,
+		Scale::uniform(13.0),
+		&font,
+		type_line,
+	);
+	text_image.save("text.png").unwrap();
+
+	// raster::save(&card, "card.png").unwrap();
 }
